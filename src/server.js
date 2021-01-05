93f7ee9a61839b0aa31d8e039d397c32dc1fd2e3
Victor Ragojos <vhsvragojos@gmail.com>
2021-01-04 16:15:19 -0800

    MOD: disable helmet
    - debugging heroku deployment errors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import morgan from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -26,7 +26,7 @@ const main = async () => {
 
 	server
 		.disable('x-powered-by')
-		.use(helmet())
+		// .use(helmet())
 		.use(cookieParser())
 		.use(express.static(process.env.RAZZLE_PUBLIC_DIR))
 		.use(
